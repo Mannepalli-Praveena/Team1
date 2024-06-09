44daaf36d2afee3b5cb31a519d8a51f83e673372
Mannepalli-Praveena <praveenasals.net@gmail.com>
2024-06-09 13:36:07 +0530

    Add files via upload

---
+++ SALS_TNPSC/src/main/java/com/sals/tnpsc/TnpscSiteApplication.java
@@ -0,0 +1,13 @@
+package com.sals.tnpsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnpscSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TnpscSiteApplication.class, args);
+	}
+
+}
