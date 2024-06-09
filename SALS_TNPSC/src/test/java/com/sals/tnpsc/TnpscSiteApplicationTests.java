a9283d1b978bd3791fb34632b9f24109d31154ce
Mannepalli-Praveena <praveenasals.net@gmail.com>
2024-06-09 18:39:57 +0530

    Add files via upload

---
+++ SALS_TNPSC/src/test/java/com/sals/tnpsc/TnpscSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sals.tnpsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TnpscSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
